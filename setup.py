4423891d1b1f1ab0d1b5e636b60b051d8a995934
neurobloomai <amarnath@neurobloom.ai>
2025-06-09 17:26:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     },
     
     name="pact-protocol",
-    version="0.1.1",
+    version="0.1.11",
     author="Neurobloom.ai",
     author_email="team@neurobloom.ai",
     description="PACT: Protocol for Agent Collaboration & Transfer",
